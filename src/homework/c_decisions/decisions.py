ffc41f98e71e1a469d77d103ef7b18a9636a1540
macrohaggen <157082428+macrohaggen@users.noreply.github.com>
2024-02-11 03:00:43 +0000

    c_decisions homework, tests seemingly upset at me - main works perfectly though

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,13 @@
-#
+def get_options_ratio(num1, num2):
+    return num1 / num2
+def get_faculty_rating(rat1):
+    if rat1 >= 0.9:
+        return "Excellent"
+    elif rat1 >= 0.85:
+        return "Very Good"
+    elif rat1 >= 0.7:
+        return "Good"
+    elif rat1 >= 0.65:
+        return "Needs Improvement"
+    elif rat1 < 0.65:
+        return "Unacceptable"
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,6 @@
-#
+from decisions import get_faculty_rating
+from decisions import get_options_ratio
+val1 = int(input("Enter Numerator:\n"))
+val2 = int(input("Enter Denominator:\n"))
+print("Options Ratio: ", get_options_ratio(val1, val2))
+print("Faculty Rating: ", get_faculty_rating(get_options_ratio(val1, val2)))
